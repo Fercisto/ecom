4c119a6e81cb0246286900ef8be597e130047403
Fercisto <dedtroll314@hotmail.com>
2025-06-09 22:47:46 -0600

    Agregando nuevo Login

--- vistas/components/header.php
+++ vistas/components/header.php
@@ -10,10 +10,7 @@
                 </div>
 
                 <div class="right-top-bar flex-w h-full">
-                    <a href="#" class="flex-c-m trans-04 p-lr-25">Help & FAQs</a>
-                    <a href="#" class="flex-c-m trans-04 p-lr-25">My Account</a>
-                    <a href="#" class="flex-c-m trans-04 p-lr-25">EN</a>
-                    <a href="#" class="flex-c-m trans-04 p-lr-25">USD</a>
+                    <a href="login.html" class="flex-c-m trans-04 p-lr-25">Login</a>
                 </div>
             </div>
         </div>
--- vistas/js/login.js
+++ vistas/js/login.js
@@ -16,72 +16,23 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
         const divRespuesta = document.getElementById("respuesta");
 
         if (nombre.trim() === "" || pass.trim() === "") {
-            divRespuesta.innerHTML = `<span style="color:red">Por favor, completa todos los campos.</span>`;
+            divRespuesta.innerHTML = `<span class="message message-error">Por favor, completa todos los campos.</span>`;
             return;
         }
 
         if (data.success) {
-            divRespuesta.innerHTML = `<span style="color: #27AE60">${data.message}</span>`;
+            divRespuesta.innerHTML = `<span class="message message-success">${data.message}</span>`;
             setTimeout(() => {
                 window.location.href = "../index.php  ";
             }, 5000);
         } else {
-            divRespuesta.innerHTML = `<span style="color: #EB5757">${data.message}</span>`;
+            divRespuesta.innerHTML = `<span class="message message-error">${data.message}</span>`;
         }
     })
     .catch(error => {
         console.error('Error al procesar la peticion:', error);
         const divRespuesta = document.getElementById("respuesta");
-        divRespuesta.innerHTML = `<span style="color:red">Error de server</span>`;
+        divRespuesta.innerHTML = `<span class="message message-error">Error de server</span>`;
     });
 })
 
-var current = null;
-document.querySelector('#nombre').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: 0,
-      duration: 700,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '240 1386',
-      duration: 700,
-      easing: 'easeOutQuart'
-    }
-  });
-});
-document.querySelector('#pass').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: -336,
-      duration: 700,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '240 1386',
-      duration: 700,
-      easing: 'easeOutQuart'
-    }
-  });
-});
-document.querySelector('#submit').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: -730,
-      duration: 700,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '530 1386',
-      duration: 700,
-      easing: 'easeOutQuart'
-    }
-  });
-});
